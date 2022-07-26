efd2fb47b42d18b2d77e7b3e363f4eade07ffb01
Deependu_Jha <deependujha21@gmail.com>
2022-07-26 17:00:30 +0530

    carousel part done in index.js page

---
+++ components/mainPage/Carousel.jsx
@@ -0,0 +1,77 @@
+import Carousel from "react-bootstrap/Carousel";
+
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/carousel/industry_4.jpg"
+          alt="First slide"
+          style={{ height: "80vh", width: "100vw" }}
+        />
+        <Carousel.Caption>
+          <h1>INNOVATE, INCULCATE and INSPIRE</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/carousel/welding_engineer.jpg"
+          alt="Second slide"
+          style={{ height: "80vh", width: "100vw" }}
+        />
+
+        <Carousel.Caption>
+          <h1>
+            Create the Unseen
+            <br />
+            Explore the unknown
+          </h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/carousel/future-work-digital-technology-economy.jpg"
+          alt="Third slide"
+          style={{ height: "80vh", width: "100vw" }}
+        />
+
+        <Carousel.Caption>
+          <h1>
+            Stretch your limits of knowledge
+            <br />
+            and create new beginnings
+          </h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/carousel/teamwork-img.jpg"
+          alt="Fourth slide"
+          style={{ height: "80vh", width: "100vw" }}
+        />
+
+        <Carousel.Caption>
+          <h1>Teamwork makes the dream work</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/carousel/steel-pipelines-cables-plant.jpg"
+          alt="Fifth slide"
+          style={{ height: "80vh", width: "100vw" }}
+        />
+
+        <Carousel.Caption>
+          <h1>Stay on the Forefront of Innovation</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default UncontrolledExample;
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import React, { useEffect } from "react";
+import Carousel from "../components/mainPage/Carousel";
 
 export default function Home({ setCurrPage }) {
   useEffect(() => {
     setCurrPage("home");
   }, []);
-  return <div className={styles.container}>Hello this is index page.</div>;
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
 }
